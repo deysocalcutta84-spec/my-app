77df3e9c6da939d391e5db1b00b537a3b946a219
deysocalcutta84-spec <deyso.calcutta84@gmail.com>
2026-02-16 01:07:34 +0530

    mon1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>🌍 Running on HTTP Port 80 🚀</h1>"
+    return "<h1>🌍 Running on HTTP Port 80 🚀 AWS</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
